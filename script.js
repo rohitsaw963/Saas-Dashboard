e2ecfd7019dc355d9a722d3808b8089e1723c79b
Your Name <you@example.com>
2024-04-30 23:27:46 +0530

    form word counter

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ console.log("This is Saas Dashboard");
 
 //Jquery syntax
 //$(selector).action(callback function)
+
+$(document).click((e) => {
+  $("#myTextarea").on("input", function () {
+    e.preventDefault();
+    let words = $(this).val().trim().split(/\s+/);
+    let characters = $(this).val().length;
+    $("#wordCount").text(words.length);
+    $("#charRemaining").text(250 - characters);
+    console.log(words);
+  });
+});
+
+
+
